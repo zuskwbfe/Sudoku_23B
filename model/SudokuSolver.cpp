230c6ad9f508a203c939ad2907c911f401892b9a
zuskwbfe <zuzubbubbu@gmail.com>
2025-05-10 00:55:20 +0300

    feat: update class SudokuSolver

--- model/SudokuSolver.cpp
+++ model/SudokuSolver.cpp
@@ -1,32 +1,49 @@
 #include "SudokuSolver.h"
+#include <algorithm>
+#include <vector>
 
 bool BacktrackingSolver::solve(SudokuBoard &board) {
   return solveRecursive(board);
 }
 
-bool BacktrackingSolver::solveRecursive(SudokuBoard &board) {
-  if (board.isSolved()) {
-    return true;
-  }
-  for (int row = 0; row < 9; ++row) {
-    for (int col = 0; col < 9; ++col) {
-      if (board.getCellValue(row, col) == 0) { // Ищем пустую клетку
-        for (int num = 1; num <= 9; ++num) {
-          if (board.isValidMove(row, col,
-                                num)) { // Проверяем можно ли поставить число
-            board.setCellValue(row, col, num); // Ставим число
+bool BacktrackingSolver::randomizedSolve(SudokuBoard &board,
+                                         std::mt19937 &gen) {
+  return solveRecursive(board, true, &gen);
+}
 
-            if (solveRecursive(board)) {
-              return true; // Верное решение
-            } else {
-              board.setCellValue(row, col,
-                                 0); // Откатываем назад и пробуем другое число
-            }
-          }
-        }
-        return false; // Нет подходящих чисел => возвращаемся назад
+bool BacktrackingSolver::solveRecursive(SudokuBoard &board, bool randomize,
+                                        std::mt19937 *gen) {
+  if (board.isSolved())
+    return true;
+
+  int row, col;
+  if (!findEmptyCell(board, row, col))
+    return false;
+
+  std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  if (randomize && gen) {
+    std::shuffle(numbers.begin(), numbers.end(), *gen);
+  }
+
+  for (int num : numbers) {
+    if (board.isValidMove(row, col, num)) {
+      board.setCellValue(row, col, num);
+      if (solveRecursive(board, randomize, gen)) {
+        return true;
+      }
+      board.setCellValue(row, col, 0);
+    }
+  }
+  return false;
+}
+
+bool BacktrackingSolver::findEmptyCell(SudokuBoard &board, int &row, int &col) {
+  for (row = 0; row < 9; row++) {
+    for (col = 0; col < 9; col++) {
+      if (board.getCellValue(row, col) == 0) {
+        return true;
       }
     }
   }
-  return true; // Судоку решено
+  return false;
 }
--- model/SudokuSolver.h
+++ model/SudokuSolver.h
@@ -1,21 +1,24 @@
 #pragma once
 
 #include "SudokuBoard.h"
+#include <random>
 
 class SudokuSolver {
 public:
-    // Стратегия (паттерн Стратегия) - интерфейс для алгоритмов решения
-    virtual bool solve(SudokuBoard& board) = 0;
-    virtual ~SudokuSolver() = default;
+  virtual bool solve(SudokuBoard &board) = 0;
+  virtual bool randomizedSolve(SudokuBoard &board, std::mt19937 &gen) = 0;
+  virtual ~SudokuSolver() = default;
 };
-
 
 // Конкретная стратегия: Backtracking (поиск с возвратом)
 // Наследуется от SudokuSolver
 class BacktrackingSolver : public SudokuSolver {
 public:
-    bool solve(SudokuBoard& board) override;
+  bool solve(SudokuBoard &board) override;
+  bool randomizedSolve(SudokuBoard &board, std::mt19937 &gen) override;
+  bool findEmptyCell(SudokuBoard &board, int &row, int &col);
 
 private:
-    bool solveRecursive(SudokuBoard& board);
+  bool solveRecursive(SudokuBoard &board, bool randomize = false,
+                      std::mt19937 *gen = nullptr);
 };
